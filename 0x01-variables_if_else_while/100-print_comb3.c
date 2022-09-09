6ae25f35318f15d95f5ec799f2bf70f3f367547e
Irakoze Christophe <ck.irakoze@gmail.com>
2022-09-09 19:59:17 +0200

    A

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints all possible combinations of two digits
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int digt1;
+	int digt2;
+
+	for (digt1 = 0; digt1 < 9; dift1++)
+	{
+		for (digt2 = digt1 + 1; dig2 < 10; digt2 )
+	}
+}
