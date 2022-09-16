2ca870d2caba1eb02743d82fae0d5a8a374bc426
Irakoze Christophe <ck.irakoze@gmail.com>
2022-09-16 17:08:09 +0200

    tasks

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - causes an infinite loop
+ * Return: Always 0
+ */
+
+int main(void)
+{
+/*	int i;*/
+	printf("Infinite loop incomming :\n");
+/*
+ *	i = 0;
+ *
+ *	while (i < 10)
+ *	{
+ *		putchar(i);
+ *	}
+ */
+	printf("Infinite loop avoided!\n");
+	return (0);
+}
