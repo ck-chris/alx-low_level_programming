5e425167452869113068620e86046ec3e9858054
Irakoze Christophe <ck.irakoze@gmail.com>
2022-09-17 13:16:05 +0200

    functions

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * main - entry point: print FizzBuzz
+ * Return: void
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 1; n <= 100; n++)
+	{
+		if ((n % 3 == 0) && (n % 5 == 0))
+			printf("FizBuzz");
+		else if (n % 5 == 0)
+			printf("Buzz");
+		else if (n % 3 == 0)
+			printf("Fizz");
+		else
+			printf("%i", n);
+		if (n < 100)
+			printf(" ");
+		else
+			printf("\n");
+	}
+	return (0);
+}
