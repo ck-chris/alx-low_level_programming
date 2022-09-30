e40fbf4bd38229b001d4d732fb63e3fcef3f5750
Irakoze Christophe <ck.irakoze@gmail.com>
2022-09-30 23:02:40 +0200

    arguments

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - print the name
+ * @argc: argv length
+ * @argv: array
+ * Return: 0 Success
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
